c48414f67ac6e2b406d8a74ea218e396e8b1547c
antoine.barre <antoine.barre@safrangroup.com>
2023-01-11 17:07:49 +0100

    feat: add fromList method
    closes #13

--- dragonfly/geography.py
+++ dragonfly/geography.py
@@ -10,13 +10,89 @@ LIST OF CLASSES:
 from dataclasses import dataclass
 
 
-@dataclass
-class Possition:
+class Position:
     """Class for the management of ECEF position
     """
-    x: float
-    y: float
-    z: float
 
-    pass
+    def __init__(self,x:float,y:float,z:float):
+        """create a Position object based on ECEF coordinates
+
+        Args:
+            x (float): x coordinates in ECEF
+            y (float): y coordinates in ECEF
+            z (float): z coordinates in ECEF
+        """
+        self.x = x
+        self.y = y
+        self.z = z
+
+    @property
+    def x(self):
+        return self._x
+
+    @x.setter
+    def x(self, value):
+        try:
+            self._x = float(value)
+        except TypeError:
+            raise TypeError('"x" shall be a scalar not a list or a tuple')
+        except ValueError:
+            raise ValueError('"x" shall be a number') from None
+
+    @property
+    def y(self):
+        return self._y
+
+    @y.setter
+    def y(self, value):
+        try:
+            self._y = float(value)
+        except TypeError:
+            raise TypeError('"y" shall be a scalar not a list or a tuple')
+        except ValueError:
+            raise ValueError('"y" shall be a number') from None
+    
+    @property
+    def z(self):
+        return self._z
+
+    @z.setter
+    def z(self, value):
+        try:
+            self._z = float(value)
+        except TypeError:
+            raise TypeError('"z" shall be a scalar not a list or a tuple')
+        except ValueError:
+            raise ValueError('"z" shall be a number') from None
+
+
+    @classmethod
+    def fromList(cls,data:list):
+
+        #check if nested list:
+        if any(isinstance(ix, list) for ix in data):
+            return Position.__fromNestedList(data)
+        else:
+            return Position.__from1DList(data)
+  
+    @classmethod
+    def __from1DList(cls,data:list):
+        """PRIVATE METHOD : create Position from a 1D list"""
+        #IO VERIFICATION
+        if not isinstance(data,list):
+            raise TypeError(f"data shall be a list  [current: {type(data)}] ")
+        if len(data)!=3:
+            raise ValueError(f"data shall be a list of 3 elements [current: {len(data)}] ")
+        
+        return Position(data[0],data[1],data[2])
+
+    @classmethod
+    def __fromNestedList(cls,datas:list):
+        """PRIVATE METHOD : create Position from a Nested list"""
+
+        if not isinstance(datas,list):
+            raise TypeError(f"data shall be a list  [current: {type(datas)}] ")
+
+        newObj = [Position.__from1DList(data) for data in datas]
+        return newObj
 
--- tests/test_Position.py
+++ tests/test_Position.py
@@ -4,19 +4,32 @@
 # CONSTANT
 
 # MODULE IMPORT
-from dragonfly.geography import Possition
+from dragonfly.geography import Position
 import pytest
 
-SIMPLE_VALUE = [10,20,30]
-
+SIMPLE_LIST     = [10,20,30]
 
 
 @pytest.fixture
 def simple_position():
-    return Possition(SIMPLE_VALUE[0],
-                      SIMPLE_VALUE[1],
-                      SIMPLE_VALUE[2])
+    return Position(SIMPLE_LIST[0],
+                      SIMPLE_LIST[1],
+                      SIMPLE_LIST[2])
     
+
+def compare_ECEF(position,X_expected):
+    """Utility function used to compare a position vs the root data"""
+
+    #extract fields
+    X = [position.x,
+            position.y,
+            position.z]
+
+    axes = ["x","y","z"]
+    for idx in range(3):
+        assert X[idx]==X_expected[idx] , f"The value of the {axes[idx]} field shall be {X_expected[idx]} [current :{X[idx]}]"
+
+############################################################################################
 
 def test_creation(simple_position) -> None:
     """Check creation of the Position object with the appropriate fields
@@ -24,8 +37,116 @@ def test_creation(simple_position) -> None:
     Args:
         simple_position (NOne): Fixture
     """
-    assert simple_position.x==SIMPLE_VALUE[0] , f"The value of the x field shall be {SIMPLE_VALUE[0]} [current :{simple_position.x}"
-    assert simple_position.y==SIMPLE_VALUE[1] , f"The value of the y field shall be {SIMPLE_VALUE[1]} [current :{simple_position.y}"
-    assert simple_position.z==SIMPLE_VALUE[2] , f"The value of the z field shall be {SIMPLE_VALUE[2]} [current :{simple_position.z}"
+       
+    #launch comparisons
+    compare_ECEF(simple_position,SIMPLE_LIST)
+
+
+def test_modif_X(simple_position):
+    """x field shall be a float or equivalent (e.g. '1', 1.0, 1"""
+
+    #Error shall be raised (bad value for the interface)
+    with pytest.raises(TypeError):
+        simple_position.x = [1,2]
+
+    with pytest.raises(TypeError):
+        simple_position.x = (1,2)
+    
+    with pytest.raises(ValueError):
+        simple_position.x = "abc"
+    
+    #No error shall be raised
+    try:
+        simple_position.x = "1.6"
+
+        assert simple_position.x ==1.6
+
+    except Exception as exc:
+        assert False, f"mutation of x raised an exception {exc}"
+
+    
+def test_modif_y(simple_position):
+    """y field shall be a float or equivalent (e.g. '1', 1.0, 1"""
+
+    #Error shall be raised (bad value for the interface)
+    with pytest.raises(TypeError):
+        simple_position.y = [1,2]
+
+    with pytest.raises(TypeError):
+        simple_position.y = (1,2)
+    
+    with pytest.raises(ValueError):
+        simple_position.y = "abc"
+    
+    #No error shall be raised
+    try:
+        simple_position.y = "1.6"
+        assert simple_position.y ==1.6
+    except Exception as exc:
+        assert False, f"mutation of y raised an exception {exc}"
+
+def test_modif_z(simple_position):
+    """z field shall be a float or equivalent (e.g. '1', 1.0, 1"""
+
+    #Error shall be raised (bad value for the interface)
+    with pytest.raises(TypeError):
+        simple_position.z = [1,2]
+
+    with pytest.raises(TypeError):
+        simple_position.z = (1,2)
+    
+    with pytest.raises(ValueError):
+        simple_position.z = "abc"
+    
+    #No error shall be raised
+    try:
+        simple_position.z = "1.6"
+        assert simple_position.z ==1.6
+    except Exception as exc:
+        assert False, f"mutation of z raised an exception {exc}"
+    
+
+
+
+############################################################################################
+
+def test_fromList_1D():
+    """fromArray method shall create a new instance from a 1D LIST"""
+    
+
+    newPosition = Position.fromList(SIMPLE_LIST)
+
+    # assess data type
+    assert isinstance(newPosition,Position) , f"the output of fromList shall be a Position object [current: {type(Position)}]"
+
+    #assessment
+    compare_ECEF(newPosition,SIMPLE_LIST)
+
+    # assess bad size
+    with pytest.raises(ValueError):
+        Position.fromList([1,2,3,4])
+
+
+
+
+def test_fromList_2D():
+    """fromArray method shall create a new instance from a 2D array"""
+    
+    Tested_Array = [SIMPLE_LIST,[2*element for element in SIMPLE_LIST], [3*element for element in SIMPLE_LIST]]
+
+    newPositions = Position.fromList(Tested_Array)
+
+    # assess data type
+    assert isinstance(newPositions,list) , f"the output of fromList shall be a Position object [current: {type(Position)}]"
+
+    #assessment
+
+    for idx in range(3):
+        compare_ECEF(newPositions[idx],Tested_Array[idx])
+
+
+
+
+
 
 
