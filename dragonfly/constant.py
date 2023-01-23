ce5e7160d8a6f3e5a7756342cce5da695af3d855
antoine.barre <antoine.barre@safrangroup.com>
2023-01-23 11:37:56 +0100

    refactor: [WIP] constant.py ok with flake8

--- dragonfly/constant.py
+++ dragonfly/constant.py
@@ -4,31 +4,33 @@
 # IMPORTED MODULES
 import math
 
+# ------------------------  EARTH MODELS  ------------------------
 
-#############################################  EARTH MODELS  #############################################
+_ELLIPSOID = ["WGS84", "SPHERICAL"]
 
-_ELLIPSOID = ["WGS84","SPHERICAL"]
-
-_ELLIPSOID_PARAMETER =[
-    {"name":"WGS84","semiMajorAxis":  6378137.0,"flattening": 1/298.257223563,"j2":1.08263E-3},
-    {"name":"SPHERICAL","semiMajorAxis":  6378137.0,"flattening": 0,"J2":0}
+_ELLIPSOID_PARAMETER = [
+    {"name": "WGS84", "semiMajorAxis": 6378137.0,
+     "flattening": 1/298.257223563, "j2": 1.08263E-3},
+    {"name": "SPHERICAL", "semiMajorAxis": 6378137.0,
+     "flattening": 0, "J2": 0}
 ]
 
 _DEFAULT_MODEL = "WGS84"
 
+
 class EarthModel():
 
-    earthRotationRate = 72.92115E-6 #rotation rate from the WGS84 rad/s
-    mu = 3.986004418E14 #m-3s-2
-   
-    def __init__(self,model=_DEFAULT_MODEL) -> None:
+    earthRotationRate = 72.92115E-6  # rotation rate from the WGS84 rad/s
+    mu = 3.986004418E14  # m-3s-2
+
+    def __init__(self, model=_DEFAULT_MODEL) -> None:
         try:
             model = model.upper()
-        except:
-            raise TypeError(f"the model value {model} is not an appropriate string")
+        except Exception as e:
+            e.add_note(f"the model value {model} is not an appropriate string")
+            raise
 
         self.model = model
-
 
     @property
     def model(self):
@@ -38,31 +40,38 @@ class EarthModel():
     def model(self, value):
         try:
             value = value.upper()
-        except:
-            raise TypeError(f"the model value {value} is not an appropriate string")
-        
+        except Exception as e:
+            e.add_note(f"the model value {value} is not an appropriate string")
+
         if value in _ELLIPSOID:
-            self._model=value.upper()
+            self._model = value.upper()
         else:
-            raise ValueError(f"the model {value.upper()} is not in the list of available ellipsoid models (ie. {_ELLIPSOID})")
-        
+            msg = (f"the model {value.upper()}"
+                   " is not in the list of available ellipsoid models"
+                   "(ie. {_ELLIPSOID})"
+                   )
+            raise ValueError(msg)
+
     @property
     def a(self):
         """_semi major axis value of the ellispoid in meters
         """
-        model_dict = next(item for item in _ELLIPSOID_PARAMETER if item["name"] == self.model)
+        model_dict = next(item for item in _ELLIPSOID_PARAMETER
+                          if item["name"] == self.model)
         return model_dict["semiMajorAxis"]
+
     @property
     def f(self):
         "flattening of the ellispoid"
-        model_dict = next(item for item in _ELLIPSOID_PARAMETER if item["name"] == self.model)
+        model_dict = next(item for item in _ELLIPSOID_PARAMETER
+                          if item["name"] == self.model)
         return model_dict["flattening"]
-    
+
     @property
     def b(self):
         """Semi minor acis of the ellispoid in meters"""
-        return   (1-self.f)*self.a
-    
+        return (1-self.f)*self.a
+
     @property
     def e(self):
         """Excentricity of the ellispoid"""
@@ -71,8 +80,6 @@ class EarthModel():
     @property
     def j2(self):
         "Second gravitationla constant"
-        model_dict = next(item for item in _ELLIPSOID_PARAMETER if item["name"] == self.model)
+        model_dict = next(item for item in _ELLIPSOID_PARAMETER
+                          if item["name"] == self.model)
         return model_dict["j2"]
-    
-
-        
